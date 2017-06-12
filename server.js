9b0cecb2cf3fad5a1182cf875f7cad086fa3aeea
sergiido <serpontin@bigmir.net>
2017-06-12 12:56:07 +0300

    Create server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const favicon = require('serve-favicon');
 const app = express();
 
 const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
-const ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+const ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 app.use(favicon(__dirname + '/public/images/favicon.ico'));
 app.set('views', __dirname + '/views');
